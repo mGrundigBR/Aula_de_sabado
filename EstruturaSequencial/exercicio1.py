8af143989c5192cd17ec0625678bf0d518a07cdd
Marcelo Grundig <marcelogrundig@gmail.com>
2019-01-26 18:34:40 +0000

    Update exercicio1.py

--- EstruturaSequencial/exercicio1.py
+++ EstruturaSequencial/exercicio1.py
@@ -23,5 +23,32 @@ print float(jan_fev + mar_abr + mai_jun + Jul_ago) / 4
 #exercicio 5
 metro = raw_input("Digite a medida em metros: ")
 centimetros = float(metro * 100)
-print("A medida em centimetros eh: %s02d" % (centimetros))
+print("A medida em centimetros eh: " + str(centimetros))
 
+#exercicio 6
+raio = raw_input("Digite a medida do raio:")
+area_circulo = 3.14 * float(raio)
+print("A area do circulo eh: " + str(area_circulo))
+
+#exercicio 7
+
+
+
+#exercicio 8
+
+#exercicio 9
+
+#exercicio 10
+
+#exercicio 11
+
+#exercicio 12
+
+#exercicio 13
+
+#exercicio 14
+
+#exercicio 15
+
+#exercicio 16
+a

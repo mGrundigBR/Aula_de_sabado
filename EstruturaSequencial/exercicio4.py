f28f98b9ab126bed36bf03fcdfe80aed6a14613d
Marcelo Grundig <marcelogrundig@gmail.com>
2019-01-26 18:57:22 +0000

    Update exercicio4.py

--- EstruturaSequencial/exercicio4.py
+++ EstruturaSequencial/exercicio4.py
@@ -3,5 +3,5 @@
 jan_fev = raw_input("Digite a nota do seu primeiro bimestre: ")
 mar_abr = raw_input("Digite a nota do seu segundo bimestre: ")
 mai_jun = raw_input("Digite a nota do seu terceiro bimestre: ")
-Jul_ago = raw_input("Digite a nota do seu quarto bimestre: ")
-print float(jan_fev + mar_abr + mai_jun + Jul_ago) / 4
+jul_ago = raw_input("Digite a nota do seu quarto bimestre: ")
+print float(jan_fev + mar_abr + mai_jun + jul_ago) / 4
